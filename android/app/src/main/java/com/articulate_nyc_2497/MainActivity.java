6be866726cfcc547ee25a5a34c0a87277a676b30
Crowdbotics <team@crowdbotics.com>
2019-04-21 00:48:13 +0000

    initial commit

---
+++ android/app/src/main/java/com/articulate_nyc_2497/MainActivity.java
@@ -0,0 +1,15 @@
+package com.articulate_nyc_2497;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "articulate_nyc_2497";
+    }
+}
